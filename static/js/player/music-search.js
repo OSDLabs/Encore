2435a1eee2250594916ede6ed45d90c3f0c22c54
Prudhvi Rampey <gergteg777@gmail.com>
2016-12-27 16:40:30 +0530

    Threw in a gorgeous search feature. :D

---
+++ static/js/player/music-search.js
@@ -0,0 +1,42 @@
+var search_button = document.getElementById("search-button");
+var overlay_search_button = document.getElementById("overlay-search-button");
+// Default search state
+var search_state = false;
+var search_overlay = document.getElementById("search-overlay");
+var content = document.getElementById("content");
+var wrapper = document.getElementById("wrapper");
+
+function activateSearch() {
+  // Show search overlay
+  search_overlay.style.display = "block";
+  // Keep search initialised with previously saved search value
+  player.updateSearch($("#input").val());
+  $('#music_ui').addClass('blur');
+}
+
+function deactivateSearch() {
+  // Hide search overlay
+  search_overlay.style.display = "none";
+  // Redirect to our main library
+  window.location.href = "/#playlist/LIBRARY";
+  $('#music_ui').removeClass('blur');
+}
+
+// Add instant search to our custom search feature
+$(".search-input").on("keyup", function (evt) {
+  player.updateSearch(evt.target.value);
+});
+
+search_button.addEventListener("click", function () {
+
+  // Display search results under our search input
+  search_overlay.appendChild(content);
+  search_state = true;
+  activateSearch();
+});
+overlay_search_button.addEventListener("click", function () {
+  search_overlay.removeChild(content);
+  wrapper.appendChild(content);
+  search_state = false;
+  deactivateSearch();
+});
--- static/js/player/sidebarview.js
+++ static/js/player/sidebarview.js
@@ -1,9 +1,18 @@
 SidebarView = Backbone.View.extend({
   template: '#sidebar_template',
-  render: function() {
-    var editable = player.playlist_collection.where({editable: true});
-    var fixed = player.playlist_collection.where({editable: false});
-    this.setElement(render(this.template, {title: 'Playlists', search: player.searchText, editable: editable, fixed: fixed}));
+  render: function () {
+    var editable = player.playlist_collection.where({
+      editable: true
+    });
+    var fixed = player.playlist_collection.where({
+      editable: false
+    });
+    this.setElement(render(this.template, {
+      title: 'Playlists',
+      search: player.searchText,
+      editable: editable,
+      fixed: fixed
+    }));
   },
 
   events: {
@@ -11,32 +20,39 @@ SidebarView = Backbone.View.extend({
     'click .search-youtube': 'searchYoutube',
     'keyup .search-input': 'searchItems',
   },
-  addPlaylist: function() {
-    bootbox.prompt('Playlist title?', function(result) {
+  addPlaylist: function () {
+    bootbox.prompt('Playlist title?', function (result) {
       if (result !== null && result !== '') {
-        socket.emit('create_playlist', {title: result, songs: []});
+        socket.emit('create_playlist', {
+          title: result,
+          songs: []
+        });
       }
     });
   },
 
-  searchItems: function() {
+  searchItems: function () {
     searchText = $('.search-input').val();
     player.updateSearch(searchText);
     return true;
   },
 
-  searchYoutube: function() {
+  searchYoutube: function () {
     var searchText = $('.search-input').val();
-    socket.emit('youtube_search', { search: searchText });
+    socket.emit('youtube_search', {
+      search: searchText
+    });
     Messenger().post('Searching youtube for songs...');
   },
 });
 
 SettingsBarView = Backbone.View.extend({
   template: '#settings_bar_template',
-  render: function() {
-    this.$el.html(render(this.template, {vol: player.getVolume()}));
-    _.defer(function() {
+  render: function () {
+    this.$el.html(render(this.template, {
+      vol: player.getVolume()
+    }));
+    _.defer(function () {
       var volElem = $('#vol_bar');
       if (volElem.length > 0) {
         player.setVolElem(volElem);
@@ -47,9 +63,12 @@ SettingsBarView = Backbone.View.extend({
   events: {
     'click #remote_setup': 'openOptions',
   },
-  openOptions: function() {
+  openOptions: function () {
     bootbox.dialog({
-      message: render('#control_template', { comp_name: player.comp_name, host: window.location.host }),
+      message: render('#control_template', {
+        comp_name: player.comp_name,
+        host: window.location.host
+      }),
       title: 'Setup Remote Control',
       buttons: {
         danger: {
@@ -59,7 +78,7 @@ SettingsBarView = Backbone.View.extend({
         success: {
           label: 'Save',
           className: 'btn-success',
-          callback: function() {
+          callback: function () {
             comp_name = $('#comp_name_input').val();
             player.setCompName(comp_name);
           },
